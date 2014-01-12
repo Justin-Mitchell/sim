0f12406765dadc6a84a5d320feff1562264f195b
Justin-Mitchell <jmitchell4140@gmail.com>
2014-01-12 13:33:35 -0800

    added *.swf

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,7 +59,7 @@ Sim::Application.configure do
 
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-  config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif *.eot *.svg *.ttf *woff *.js)
+  config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif *.eot *.svg *.ttf *woff *.js *.swf)
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
